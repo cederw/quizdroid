b263a4428c575beea4f887597cb098cf05df86d2
iGuest <sd@fd.com>
2015-05-20 00:39:19 -0700

    sds

--- app/src/main/java/edu/washington/cederw/quizdroid/getQuestions.java
+++ app/src/main/java/edu/washington/cederw/quizdroid/getQuestions.java
@@ -4,9 +4,11 @@ import android.app.DownloadManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.net.Uri;
 import android.net.http.AndroidHttpClient;
 import android.os.Environment;
+import android.provider.Settings;
 import android.util.Log;
 import android.widget.Toast;
 
@@ -24,11 +26,29 @@ public class getQuestions extends BroadcastReceiver {
 // TODO: This method is called when the BroadcastReceiver is receiving
 // an Intent broadcast.
         Log.i("uhh", "got it");
-        String message = intent.getStringExtra("url");
 
-        dm = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
-        DownloadManager.Request request = new DownloadManager.Request(Uri.parse(message));
-        enqueue = dm.enqueue(request);
+        try
+        {
+            ConnectivityManager cm = (ConnectivityManager)context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            cm.getActiveNetworkInfo().isConnectedOrConnecting();
+
+            String message = intent.getStringExtra("url");
+
+            dm = (DownloadManager) context.getSystemService(Context.DOWNLOAD_SERVICE);
+            DownloadManager.Request request = new DownloadManager.Request(Uri.parse(message));
+            request.setDestinationInExternalFilesDir(context, Environment.DIRECTORY_DOWNLOADS, "data.json");
+            enqueue = dm.enqueue(request);
+        }
+        catch (Exception e)
+        {
+            Toast.makeText(context, "It seems you are offline :(", Toast.LENGTH_LONG).show();
+            if(Settings.System.getInt(context.getContentResolver(), Settings.Global.AIRPLANE_MODE_ON, 0) != 0){
+                Intent intent2 = new Intent(Settings.ACTION_AIRPLANE_MODE_SETTINGS );
+                intent2.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                context.startActivity(intent2);
+            }
+        }
+
         //Toast.makeText(context, message, Toast.LENGTH_LONG).show();
     }
 }
